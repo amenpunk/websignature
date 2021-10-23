560e422991ca87c78601ecaeeef864b2ac2a0800
amenpunk <ondasycircuitos@gmail.com>
2021-10-23 17:02:07 -0600

    Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router } from "react-router-dom";
+import { initializeApp } from 'firebase/app';
+
 
 import Container from './components/container';
 import { Header } from './components/Header'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBlbF796CnVCBnZSAjcSTlzMrJnaPTZmhg",
+    authDomain: "api-tester-dd087.firebaseapp.com",
+    databaseURL: "https://api-tester-dd087.firebaseio.com",
+    projectId: "api-tester-dd087",
+    storageBucket: "api-tester-dd087.appspot.com",
+    messagingSenderId: "1081588619626",
+    appId: "1:1081588619626:web:4f8063c2085ce2e35bad13",
+    measurementId: "G-S37X0MGZJL"
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,48 @@
-import react from 'react'
+import react, { useRef } from 'react'
 import { Row, Col, Form, Button } from 'react-bootstrap';
+import { 
+    getAuth,
+    createUserWithEmailAndPassword, 
+    GoogleAuthProvider,
+    signInWithPopup
+} from 'firebase/auth';
+const provider = new GoogleAuthProvider();
 
 export class Login extends react.Component{
+
     constructor(props){
         super(props)
+        this.auth = getAuth()
+    }
+
+    inWithGoogle = () => {
+        const auth = getAuth();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    }
+
+    componentDidMount() {
+
+    }
+
+    Email = (event) => {
+        console.log(event)
     }
 
     render (){
@@ -13,16 +52,22 @@ export class Login extends react.Component{
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Correo</Form.Label>
-                            <Form.Control type="email" placeholder="Ingresa tu correo" />
+                            <Form.Control onChange={this.Email} type="email" placeholder="Ingresa tu correo" />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Contrasena</Form.Label>
-                            <Form.Control type="password" placeholder="Contrasena" />
+                            <Form.Control  type="password" placeholder="Contrasena" />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
+
+                        <Button variant="light" type="submit">
                             Ingresar
                         </Button>
+                        
+                        <Button style={{ paddinLeft : 15, marginLeft : 15 }} onClick={this.inWithGoogle} variant="danger">
+                            Ingresar con Google
+                        </Button>
+
                     </Form>
                 </Col>
             </Row>
